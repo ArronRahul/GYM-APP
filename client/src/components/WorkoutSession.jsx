29f4a1489f75d80a7753ed7b740364b05777ad0d
rahulap-aot <rahul.ap@aot-technologies.com>
2024-10-13 23:29:24 -0700

    app.js changed
    chnaged app.js lazy loading

--- client/src/components/WorkoutSession.jsx
+++ client/src/components/WorkoutSession.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const WorkoutSession = () => {
   return (
-    <div>
-      
+    <section className='workout_session'>
+      <div className='wrapper'>
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+        quisquam. Eaque alias, blanditiis quidem maiores illo unde ipsam!</p>
+        <img src='/img5.jpg' alt='workout'/>
     </div>
+    <div className='wrapper'>
+      <h1>FEATURED BOOTCAMPS</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+      impedit quasi sunt amet rerum accusamus odio eveniet unde? </p>
+      <div className="bootcamps">
+        <div>
+          <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+        </div>
+      </div>
+    </div>
+    </section>
   )
 }
 
