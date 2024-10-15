7d486ee5da92feaeb5a4c51222086509d0f97d4c
rahulap-aot <rahul.ap@aot-technologies.com>
2024-10-15 05:24:11 -0700

    importing changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { ToastContainer, toast } from 'react-toastify';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import WorkoutSession from './components/WorkoutSession';
-import Gallery from './components/Gallery';
-import Pricing from './components/Pricing';
-import Contact from './components/Contact';
-import BMICal from './components/BMICal';
-import Footer from './components/Footer';
+import {
+  Navbar,
+  Hero,
+  WorkoutSession,
+  Gallery,
+  Pricing,
+  Contact,
+  BMICal,
+  Footer
+} from './components';
 import './App.css';  
 
 const App = () => {
@@ -24,9 +26,9 @@ const App = () => {
       <BMICal />
       <Footer />
       
-      <ToastContainer theme='dark' position='top-center' />
+      <ToastContainer theme="dark" position="top-center" />
     </Router>
-  )
-}
+  );
+};
 
 export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navbar } from './Navbar';
+export { default as Hero } from './Hero';
+export { default as WorkoutSession } from './WorkoutSession';
+export { default as Gallery } from './Gallery';
+export { default as Pricing } from './Pricing';
+export { default as Contact } from './Contact';
+export { default as BMICal } from './BMICal';
+export { default as Footer } from './Footer';
